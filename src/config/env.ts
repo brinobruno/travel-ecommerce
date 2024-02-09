0a3c8e4e3ef4ba32cbaeb849ae7883a8076b6847
Bruno Correa <brunosantos6ft@gmail.com>
2024-02-09 19:51:29 -0300

    chore: Update .env

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   NEXT_PUBLIC_API_URL: z.string(),
+  NEXT_PUBLIC_CURRENCY_API_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
