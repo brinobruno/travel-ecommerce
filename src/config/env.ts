2534af10a3bfe51404a8b026bc46c7e625b7ef92
Bruno Correa <brunosantos6ft@gmail.com>
2024-02-09 16:16:49 -0300

    chore: Update env naming

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
-  MODE: z.enum(['production', 'development', 'test']),
-  API_URL: z.string(),
+  NEXT_PUBLIC_API_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { env } from '@/src/config/env'
 
 export const api = axios.create({
-  baseURL: env.API_URL,
+  baseURL: env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
   timeout: 1000,
 })
