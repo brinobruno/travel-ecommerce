f45cadf0e8ba4a48219eff6474f65bcd82ba1e9a
Bruno Correa <brunosantos6ft@gmail.com>
2024-02-09 19:50:39 -0300

    chore: Update dependencies

--- src/lib/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-import { env } from '@/src/config/env'
-
-export const api = axios.create({
-  baseURL: env.NEXT_PUBLIC_API_URL,
-  withCredentials: true,
-  timeout: 1000,
-})
