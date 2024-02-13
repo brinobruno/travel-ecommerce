84a919f584937438cf456be23f25cdc32623f25f
Bruno Correa <brunosantos6ft@gmail.com>
2024-02-13 16:44:31 -0300

    chore: Add base cart modal

--- src/components/CartDrawer/index.tsx
+++ src/components/CartDrawer/index.tsx
@@ -1,18 +1,53 @@
+'use client'
+
 import { ShoppingCart } from 'lucide-react'
+import { useEffect, useRef, useState } from 'react'
 import { useTheme } from 'styled-components'
 
 import { CartAmount, Container } from './styles'
 
 export function CartDrawer() {
   const theme = useTheme()
+  const [isCartOpen, setIsCartOpen] = useState(false)
+  const cartElement = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e: Event) => {
+      if (
+        cartElement.current &&
+        !cartElement.current.contains(e.target as Node)
+      ) {
+        setIsCartOpen(false)
+      }
+    }
+
+    document.addEventListener('click', checkIfClickedOutside)
+    return () => {
+      document.removeEventListener('click', checkIfClickedOutside)
+    }
+  }, [isCartOpen])
+
+  const handleContainerClick = () => {
+    setIsCartOpen(!isCartOpen)
+  }
 
   return (
-    <Container>
+    <Container onClick={handleContainerClick} isOpen={isCartOpen}>
       <ShoppingCart color={theme.colors.white} fill={theme.colors.white} />
 
       <CartAmount>
         <span>0 </span>
       </CartAmount>
+
+      <div
+        className="cart-modal"
+        ref={cartElement}
+        onClick={(e) => {
+          e.stopPropagation() // Prevent cart closing if clicked inside
+        }}
+      >
+        <span>Opened</span>
+      </div>
     </Container>
   )
 }
--- src/components/CartDrawer/styles.ts
+++ src/components/CartDrawer/styles.ts
@@ -1,8 +1,12 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { FlexCenter } from '../../styles/shared'
 
-const Container = styled.button`
+interface CartDrawerProps {
+  isOpen: boolean
+}
+
+const Container = styled.div<CartDrawerProps>`
   background: ${({ theme }) => theme.colors.labelPrimary};
 
   height: 50px;
@@ -14,6 +18,33 @@ const Container = styled.button`
   align-items: center;
   justify-content: center;
   gap: 12px;
+
+  position: relative;
+
+  .cart-modal {
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+
+    color: ${({ theme }) => theme.colors.gray100};
+    background: ${({ theme }) => theme.colors.white};
+    box-shadow: ${({ theme }) => theme.boxShadow.m};
+
+    padding: 24px;
+
+    position: absolute;
+    bottom: -48px;
+    right: 0;
+
+    z-index: 100;
+
+    ${(props) =>
+      props.isOpen &&
+      css`
+        visibility: visible;
+        opacity: 1;
+      `}
+  }
 `
 
 const CartAmount = styled(FlexCenter)`
